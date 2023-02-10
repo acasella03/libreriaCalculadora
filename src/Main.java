6e21b0a1a42f72fc4789fa00f0b433aa9af9edb8
Angi Casella <acasella@danielcastelao.org>
2023-02-10 16:35:43 +0100

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import com.angi.Io.EntradaSalida;
+import com.angi.calculadora.Calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+        int opcion;
+        Float numero1, numero2, resultado;
+
+        opcion = EntradaSalida.entradaEntero("***MENU*** \n\n 1---> SUMA \n 2---> RESTA \n 3---> MULTIPLICACIÓN \n 4---> DIVISIÓN \n\n Teclea una opción de las anteriores");
+        numero1 = EntradaSalida.entradaFloat("Escribe el primer número: ");
+        numero2 = EntradaSalida.entradaFloat("Escribe el segundo número: ");
+
+        if (numero1 == null || numero2 == null) {
+            EntradaSalida.salida("Debes escribir un número, no una letra", EntradaSalida.SALIDA_CONSOLA);
+        } else {
+            if (opcion >= 1 && opcion <= 4) {
+                resultado = Calculadora.calculadora(numero1, numero2, opcion);
+                if (Float.isInfinite(resultado)) {
+                    EntradaSalida.salida("Dividir entre cero es infinito, prueba con otro número", EntradaSalida.SALIDA_CONSOLA);
+                } else {
+                    EntradaSalida.salida("El resultado de la opción seleccionada es: " + resultado, EntradaSalida.SALIDA_CONSOLA);
+                }
+            }
+        }
+    }
+}
---
+++ src/com/angi/calculadora/Calculadora.java
@@ -0,0 +1,78 @@
+package com.angi.calculadora;
+
+/**
+ * Librería Calculadora Básica
+ *
+ * @author Angi Casella
+ * @version 1.0
+ */
+
+public class Calculadora {
+
+    /**
+     * opción para calcular una suma
+     */
+    public static final int SUMA = 1;
+    /**
+     * opción para calcular una resta
+     */
+    public static final int RESTA = 2;
+    /**
+     * opción para calcular una multiplicación
+     */
+    public static final int MULTIPLICACION = 3;
+    /**
+     * opción para calcular una división
+     */
+    public static final int DIVISION = 4;
+    /**
+     * guarda el resultado de la operación
+     */
+    private static Float resultado = null;
+
+    /**
+     * Calculadora que realiza operación de suma, resta, multiplicación o división entre dos números
+     *
+     * @param num1   primer número para el cálculo de la opereción seleccionada
+     * @param num2   segundo número para el cálculo de la opereción seleccionada
+     * @param option tipo de operación a seleccionar por el usuario
+     * @return el resultado de la operación seleccionada por el usuario
+     */
+    public static Float calculadora(Float num1, Float num2, int option) {
+        switch (option) {
+            case SUMA:
+                try {
+                    resultado = num1 + num2;
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+                break;
+
+            case RESTA:
+                try {
+                    resultado = num1 - num2;
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+                break;
+            case MULTIPLICACION:
+                try {
+                    resultado = num1 * num2;
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+                break;
+            case DIVISION:
+                try {
+                    resultado = num1 / num2;
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+                break;
+            default:
+                System.out.println("Fuera del rango de opciones");
+        }
+
+        return resultado;
+    }
+}
